e5ed7a6c33ba6f6fb7b81624ef1ca308e795f72a
Lille Aila <67327023+LilleAila@users.noreply.github.com>
2020-07-31 19:37:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function dd(date2, date1) {
     utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
     utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
 
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+    return Math.floor((utc1 - utc2) / _MS_PER_DAY);
 }
 // Write Javascript code!
 
